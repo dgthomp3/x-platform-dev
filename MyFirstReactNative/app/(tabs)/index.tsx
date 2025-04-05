d2fd66d4230a73e38022d034f713a39c0cb608ab
Dallas Thompson III <dgthomp96@gmail.com>
2025-04-05 18:01:33 -0400

    App.tsx baked into homescreen

--- MyFirstReactNative/app/(tabs)/index.tsx
+++ MyFirstReactNative/app/(tabs)/index.tsx
@@ -1,30 +1,11 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import { View } from 'react-native';
 import React from "react";
-import { View, Text } from "react-native";
+import App from './App';
 
 export default function HomeScreen() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center'} }>
-      <Text>Hello, React Native!</Text>
+    <View style={{ flex: 1 } }>
+      <App />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
-});
