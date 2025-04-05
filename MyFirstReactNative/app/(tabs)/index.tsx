652891c18622ccbed4da073c2f6e5949619efb8d
Dallas Thompson III <dgthomp96@gmail.com>
2025-04-05 17:38:46 -0400

    expo project

---
+++ MyFirstReactNative/app/(tabs)/index.tsx
@@ -0,0 +1,30 @@
+import { Image, StyleSheet, Platform } from 'react-native';
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function HomeScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center'} }>
+      <Text>Hello, React Native!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  titleContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 8,
+  },
+  stepContainer: {
+    gap: 8,
+    marginBottom: 8,
+  },
+  reactLogo: {
+    height: 178,
+    width: 290,
+    bottom: 0,
+    left: 0,
+    position: 'absolute',
+  },
+});
